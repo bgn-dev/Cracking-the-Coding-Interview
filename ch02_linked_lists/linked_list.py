00a8baad80ff0ba8186b04d37db3d7f80fcec266
bgn-dev <81875423+bgn-dev@users.noreply.github.com>
2025-10-21 10:44:16 +0200

    Add: Comments and changed linked list data set

--- ch02_linked_lists/linked_list.py
+++ ch02_linked_lists/linked_list.py
@@ -3,11 +3,13 @@ class Node:
         self.data = data
         self.next = None
 
+# Insert a new node at the end of the linked list
 def insert(head: Node, data):
     while head.next != None:
         head = head.next
     head.next = Node(data)
 
+# Delete first node found with the same data
 def delete(head: Node, data) -> Node:
     n = head
     if n.data == data:
@@ -19,7 +21,7 @@ def delete(head: Node, data) -> Node:
             return head
         n = n.next
 
-
+# Print the linked list in it's row
 def traverse_linked_list(head: Node):
     while head != None:
         print(head.data)
@@ -27,10 +29,10 @@ def traverse_linked_list(head: Node):
         
 
 if __name__ == '__main__':
-    head = Node(5)
-    insert(head, 6)
-    insert(head, 9)
-    insert(head, 1)
-    insert(head, 23)
-    
-    traverse_linked_list(delete(head, 6))
+    head = Node(1)
+    insert(head,5)
+    insert(head,2)
+    insert(head,13)
+    insert(head,7)
+    insert(head,3)    
+    traverse_linked_list(delete(head,5))

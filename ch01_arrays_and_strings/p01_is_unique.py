6e584b2135b69938e92602c5d943309f7d9f8c19
bgn-dev <81875423+bgn-dev@users.noreply.github.com>
2025-11-11 11:25:42 +0100

    Chapter 01, p01: Add function description

--- ch01_arrays_and_strings/p01_is_unique.py
+++ ch01_arrays_and_strings/p01_is_unique.py
@@ -1,4 +1,8 @@
 def is_unique_brute_force(string: str) -> bool:
+    """
+    Checks whether or not a string is unique using a brute force method.
+    Returns True or False, accordingly.
+    """
     for i in range(len(string)):
         for j in range(i+1,len(string)):
             if string[i] == string[j]:
@@ -6,9 +10,17 @@ def is_unique_brute_force(string: str) -> bool:
     return True
 
 def hash_function(character: str, table_size: int) -> int:
+    """
+    Compute the hash number which is used for the index within the hash table.
+    """
     return ord(character) % table_size
 
 def is_unique_hash_table(string: str) -> bool:
+    """
+    Checks whether or not a string is unique using a hash table.
+    Disclaimer: Not (significantly) faster than the brute force method.
+    Returns True or False, accordingly.
+    """
     table_size = 100
     tables = [[] for _ in range(table_size)]
     for character in string:
@@ -20,6 +32,11 @@ def is_unique_hash_table(string: str) -> bool:
     return True
 
 def is_unique_bit_vector(string: str) -> bool:
+    """
+    Checks whether or not a string is unique using a bit vector.
+    Unicode of character used for position shift of the mask which is compared to the bit_vector.
+    Returns True or False, accordingly.
+    """
     bit_vector = 0 # 000...0
     for character in string:
         position = ord(character) # unicode of character
@@ -30,7 +47,9 @@ def is_unique_bit_vector(string: str) -> bool:
     return True
 
 def is_unique_sort_and_compare(string: str) -> bool:
-    # Sort string and compare each neighbour with each other
+    """
+    Checks whether or not a string is unique sorting a string and comparing each neighboring character.
+    """
     string = sorted(string)
     for i in range(len(string)-1):
         if string[i] == string [i+1]:
